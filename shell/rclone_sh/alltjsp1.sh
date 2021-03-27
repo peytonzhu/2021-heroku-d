19532bc28ccf9cef7cb63b4adb76ffe07af322a6
peytonzhu <48082092+peytonzhu@users.noreply.github.com>
2021-03-27 20:38:50 +0800

    Update alltjsp1.sh

--- shell/rclone_sh/alltjsp1.sh
+++ shell/rclone_sh/alltjsp1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #=================================================
-rclone copy /home/downloads tjsp1:/hero
+rclone copy /home/downloads tjsp1:/hero --exclude "*.torrent"  --min-size 10
