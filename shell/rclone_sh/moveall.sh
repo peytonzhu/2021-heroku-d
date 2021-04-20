9671bd41b17e0b34f4d1569930be88fe22b71a53
peytonzhu <48082092+peytonzhu@users.noreply.github.com>
2021-04-20 23:11:22 +0800

    Update moveall.sh

--- shell/rclone_sh/moveall.sh
+++ shell/rclone_sh/moveall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #=================================================
-sleep 10
-rclone copy /home/downloads ensp1:/rclone --exclude "*.torrent"  --min-size 10
-sleep 10
-rclone copy /home/downloads ensp1:/rclone --exclude "*.torrent"  --min-size 10
+sleep 2
+str1="/home/"
+str2=`date +%H_%M_%S`
+str3=$str1$str2
+mkdir $str3
+mv /home/downloads/* $str3
+rclone copy $str3 ensp1:/rclone --exclude "*.torrent"  --min-size 10
+sleep 2
+rclone copy $str3 ensp1:/rclone --exclude "*.torrent"  --min-size 10
